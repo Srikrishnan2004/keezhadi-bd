855827cdf112bfacc429b86b8fc5166bf790d47e
Srikrishnan2004 <velayuthamsrikrishnan@gmail.com>
2025-01-28 22:01:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
